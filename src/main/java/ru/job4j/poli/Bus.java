6891993dadccd8390d020d6e6ab4ea74f6758ba9
Stepan Bogdanov <bogstepan@gmail.com>
2021-02-09 11:06:26 +0300

    3. Интерфейс Input [#448373]

---
+++ src/main/java/ru/job4j/poli/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poli;
+
+public class Bus implements Transport{
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int number) {
+
+    }
+
+    @Override
+    public float refuel(float fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poli/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poli;
+
+public interface Transport {
+
+    void drive();
+
+    void passengers(int number);
+
+    float refuel(float fuel);
+}
