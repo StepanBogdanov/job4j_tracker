b95ac24bca519b09593be9a9a55df5d5404c6ad9
Stepan Bogdanov <bogstepan@gmail.com>
2021-02-09 21:40:57 +0300

    4.2. Статические методы. [#448378]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,37 +2,49 @@ package ru.job4j.tracker;
 
 public class StartUI {
 
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void replaceItem(Input input, Tracker tracker) {
+        int id = input.askInt("Enter id: ");
+        String name = input.askStr("Enter new item's name: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Item has been replaced");
+        } else {
+            System.out.println("Id not found");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        int id = input.askInt("Enter id: ");
+        if (tracker.delete(id)) {
+            System.out.println("Item has been deleted");
+        } else {
+            System.out.println("Id not found");
+        }
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             int select = input.askInt("Select: ");
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
                 Item[] all = tracker.findAll();
                 for (Item item : all) {
                     System.out.println(item);
                 }
             } else if (select == 2) {
-                int id = input.askInt("Enter id: ");
-                String name = input.askStr("Enter new item's name: ");
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Item has been replaced");
-                } else {
-                    System.out.println("Id not found");
-                }
+                StartUI.replaceItem(input, tracker);
             } else if (select == 3) {
-                int id = input.askInt("Enter id: ");
-                if (tracker.delete(id)) {
-                    System.out.println("Item has been deleted");
-                } else {
-                    System.out.println("Id not found");
-                }
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
                 int id = input.askInt("Enter id: ");
                 Item item = tracker.findById(id);
