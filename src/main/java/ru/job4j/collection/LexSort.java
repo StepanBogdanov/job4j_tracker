081de0cb9cb2a918ffa1b6ec67824e842101927d
Stepan Bogdanov <bogstepan@gmail.com>
2021-02-22 21:47:49 +0300

    4. Сортировка номера [#448345]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,8 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        String[] leftArray = left.split(". ");
-        String[] rightArray = right.split(". ");
+        String[] leftArray = left.split("\\.");
+        String[] rightArray = right.split("\\.");
         return Integer.compare(Integer.valueOf(leftArray[0]), Integer.valueOf(rightArray[0]));
     }
 }
