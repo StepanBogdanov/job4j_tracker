94719ecef440e4da0401dfa55a8394ee7a928f82
Stepan Bogdanov <bogstepan@gmail.com>
2021-03-01 13:34:39 +0300

    6. Функции высшего порядка [#448169]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,7 +1,6 @@
 package ru.job4j.search;
 
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
@@ -14,28 +13,12 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        Predicate<Person> combine = new Predicate<Person>() {
-            @Override
-            public boolean test(Person person) {
-                boolean rsl = false;
-                Field[] fields = person.getClass().getDeclaredFields();
-                for (Field field : fields) {
-                    field.setAccessible(true);
-                    String value = null;
-                    try {
-                        value = (String) field.get(person);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                    if (value.equals(key)) {
-                        rsl = true;
-                        break;
-                    }
-                }
-                return rsl;
-            }
-        };
         ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> checkName = p -> p.getName().equals(key);
+        Predicate<Person> checkSurname = p -> p.getSurname().equals(key);
+        Predicate<Person> checkPhone = p -> p.getPhone().equals(key);
+        Predicate<Person> checkAddress = p -> p.getAddress().equals(key);
+        Predicate<Person> combine = checkName.or(checkSurname.or(checkPhone.or(checkAddress)));
         for (Person person : persons) {
             if (combine.test(person)) {
                 result.add(person);
